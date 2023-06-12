7d01e1915ae3ae7abc5abf3a84e24036aa53c0bb
Alexander Ryzhov <aryzhov84@gmail.com>
2023-06-12 07:12:03 +0300

    add water

---
+++ Water.java
@@ -0,0 +1,10 @@
+package Seminar2OOP_Homework;
+
+public class Water implements AquaSoundAble {
+
+    @Override
+    public String sound() {
+        return "Water is purring and murmuring.";
+    }
+}
+
