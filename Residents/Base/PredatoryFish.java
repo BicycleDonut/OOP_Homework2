740be54781f8d10a97c9d15e2c598fc915fee509
Alexander Ryzhov <aryzhov84@gmail.com>
2023-06-12 10:41:54 +0300

    Pre...fish

---
+++ Residents/Base/PredatoryFish.java
@@ -0,0 +1,17 @@
+package OOP_HW02_Aquarium.Residents.Base;
+
+import Seminar2OPP_Homework.EatingFish;
+
+public abstract class PredatoryFish extends Fishes implements EatingFish {
+
+    public PredatoryFish(String type, String name) {
+        super(type, name);
+    }
+
+    @Override
+    public String feed() {
+        return "Blop-blop-blop.(In meaning \"I like to eat other fishes\")";
+    }
+
+}
+
