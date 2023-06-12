d7cd79019dce558b4b3f0ba141f88accf9fb60af
Alexander Ryzhov <aryzhov84@gmail.com>
2023-06-12 10:36:44 +0300

    Her...fish

---
+++ Residents/Base/HerbivorousFish.java
@@ -0,0 +1,11 @@
+package Seminar2OOP_Homework.Residents.Base;
+
+public abstract class HerbivorousFish extends Fishes {
+
+    public HerbivorousFish(String type, String name) {
+        super(type, name);
+    }
+}
+
+    
+
